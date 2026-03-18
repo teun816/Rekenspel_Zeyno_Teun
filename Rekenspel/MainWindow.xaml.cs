5e6d24d2c230c45ac3df7d96bfb7b0e29de603a1
teun816 <teunvandenouden106@gmail.com>
2026-03-18 11:17:02 +0100

    new

--- Rekenspel/MainWindow.xaml.cs
+++ Rekenspel/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Rekenspel
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
